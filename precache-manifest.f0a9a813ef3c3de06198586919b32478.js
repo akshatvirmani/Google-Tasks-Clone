b163b71318c60bb94da8033d191082eb9c04a4e5
Akshat Virmani <akshatvirmani93@gmail.com>
2021-11-01 18:48:16 +0530

    Updates

---
+++ precache-manifest.f0a9a813ef3c3de06198586919b32478.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ee978691df1292aee447d249e58f76",
+    "url": "/Google-Tasks-Clone/index.html"
+  },
+  {
+    "revision": "ca0a4e739db18a588584",
+    "url": "/Google-Tasks-Clone/static/css/main.a3516c84.chunk.css"
+  },
+  {
+    "revision": "78221b91be68d1f1ae6d",
+    "url": "/Google-Tasks-Clone/static/js/2.cd030909.chunk.js"
+  },
+  {
+    "revision": "ca0a4e739db18a588584",
+    "url": "/Google-Tasks-Clone/static/js/main.9b1e725e.chunk.js"
+  },
+  {
+    "revision": "131d73e445b9e52bc970",
+    "url": "/Google-Tasks-Clone/static/js/runtime-main.7bf904b5.js"
+  },
+  {
+    "revision": "81abc80b12d2abd34bf7ff27d972e1c8",
+    "url": "/Google-Tasks-Clone/static/media/berger.81abc80b.png"
+  },
+  {
+    "revision": "405dbbd49236d3a4aa5a73e0e7685629",
+    "url": "/Google-Tasks-Clone/static/media/intro-splash.405dbbd4.png"
+  }
+]);
